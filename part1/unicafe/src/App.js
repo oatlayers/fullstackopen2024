20b708a00d716ba2c1b06f9e7198b3abb29c1e79
oatlayers <pullherehfree@gmail.com>
2023-03-12 00:05:11 +0700

    worked on unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from 'react'
+const Statistics = (props) => {
+  if (props.value === 0 || isNaN(props.value) === true) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {props.text} {props.value}
     </div>
-  );
+  )
+}
+ 
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+
+  return (
+    <div>
+      <h1>give feedback</h1><br/>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h1>statistics</h1>
+
+      <Statistics text='good' value={good}/>
+      <Statistics text='neutral' value={neutral}/>
+      <Statistics text='bad' value={bad}/>
+      <Statistics text='all' value={good + neutral + bad}/>
+      <Statistics text='average' value={(good + neutral*0 + bad*-1)/(good + neutral + bad)}/>
+      <Statistics text='positive' value={(good/ (good + neutral + bad))*100}/>
+    </div>
+  )
 }
 
-export default App;
+export default App
