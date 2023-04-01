2501af6398d5f3192d558cb9e1553d9403805031
oatlayers <71181864+oatlayers@users.noreply.github.com>
2023-04-01 11:03:48 +0700

    finished until 1.11 unicafe

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,8 +11,8 @@ const Part = (props) => {
   return (
     <div>
       <p>{props.part1} {props.exercises1}</p>
-      <p> {props.part2} {props.exercises2} </p>
-      <p> {props.part3} {props.exercises3} </p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
     </div>
 
   )
@@ -42,19 +42,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
       <div>
-          <Header course = 'Half Stack application development'/>
-          <Part part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3}/>
+          <Header course = {course.name}/>
+          <Part part1 = {course.parts[0].name} exercises1 = {course.parts[0].exercises} part2 = {course.parts[1].name} exercises2 = {course.parts[1].exercises} part3 = {course.parts[2].name} exercises3 = {course.parts[2].exercises}/>
           <Content/>
-          <Total exercises = {exercises1 + exercises2 + exercises3}/>
+          <Total exercises = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
       </div>
   )
 }
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,51 +1,71 @@
 import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleCLick}>
+      {props.text}
+    </button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>{props.text} {props.value} {props.percent}</div>
+  )
+}
+
 const Statistics = (props) => {
-  if (props.value === 0 || isNaN(props.value) === true) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
+  if (props.good + props.neutral + props.bad > 0) {
+    return (      
+        <table>
+          <tr>
+            <td><StatisticLine text = 'good' /></td>
+            <td>{props.good}</td>
+          </tr>
+          <tr>
+            <td><StatisticLine text = 'neutral' /></td>
+            <td>{props.neutral}</td>
+          </tr>
+          <tr>
+            <td><StatisticLine text = 'bad' /></td>
+            <td>{props.bad}</td>
+          </tr>
+          <tr>
+            <td><StatisticLine text = 'all' /></td>
+            <td>{props.good + props.neutral + props.bad}</td>
+          </tr>
+          <tr>
+            <td><StatisticLine text = 'average' /></td>
+            <td>{(props.good*1 + props.bad*-1) / (props.good + props.neutral + props.bad)}</td>
+          </tr>
+          <tr>
+            <td><StatisticLine text = 'positive'  /></td>
+            <td>{(props.good / (props.good + props.neutral + props.bad))*100} %</td>
+          </tr>
+        </table>
     )
   }
   return (
-    <div>
-        {props.text} {props.value}
-    </div>
+    <div>No feedback given</div>
   )
 }
- 
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGood = () => {
-    setGood(good + 1)
-  }
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const handleBad = () => {
-    setBad(bad + 1)
-  }
-
-
   return (
     <div>
-      <h1>give feedback</h1><br/>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
-      <h1>statistics</h1>
+      <h1>give feedback</h1>
+      <Button handleCLick = {() => setGood(good + 1)} text = 'good'/>
+      <Button handleCLick = {() => setNeutral(neutral + 1)} text = 'neutral'/>
+      <Button handleCLick = {() => setBad(bad + 1)} text = 'bad'/>
 
-      <Statistics text='good' value={good}/>
-      <Statistics text='neutral' value={neutral}/>
-      <Statistics text='bad' value={bad}/>
-      <Statistics text='all' value={good + neutral + bad}/>
-      <Statistics text='average' value={(good + neutral*0 + bad*-1)/(good + neutral + bad)}/>
-      <Statistics text='positive' value={(good/ (good + neutral + bad))*100}/>
+      <h1>statistics</h1>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div>
+
   )
 }
 
