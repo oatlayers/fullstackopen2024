4a7cc48c1924962c0a26a8c81f0f654cf22e0e82
oatlayers <71181864+oatlayers@users.noreply.github.com>
2023-04-02 07:45:03 +0700

    finished all part 1 exercises

--- part1/anecdote/src/App.jsx
+++ part1/anecdote/src/App.jsx
@@ -1,79 +1,45 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const getRandomInt = () => setSelected(Math.floor(Math.random() * 8))
+   
+  const [counters, setCounters] = useState(new Array(8).fill(0))
+  
+  const handleVote = () => {
+    const newCounters = [...counters]
+    newCounters[selected] += 1
+    setCounters(newCounters)  
   }
 
-  const Header = () => {
-    return (
-      <h1>
-          <p>{course.name}</p>
-      </h1>
-    )
+  const findLargest = Math.max(...counters)
+  const largestVote = counters.indexOf(findLargest)
 
-  }
-  const Part1 = () => {
-    return (
-      <div>
-          <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-      </div>
-    )
-
-  }
-  const Part2 = () => {
-    return (
-      <div>
-          <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-      </div>
-    )
-
-  }
-  const Part3 = () => {
-    return (
-      <div>
-          <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-      </div>
-    )
-
-  }
-  const Content = () => {
-    return (
-      <div>
-          <Part1 />
-          <Part2 />
-          <Part3 />
-      </div>
-    )
-
-  }
-  const Total = () => {
-    return (
-      <div>
-          <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-      </div>
-    )
-
-  }
   return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <div>has {counters[selected]} votes</div>
       <div>
-          <Header />
-          <Content />
-          <Total />
+        <button onClick={handleVote}>vote</button>
+        <button onClick={getRandomInt}>next anecdote</button>
       </div>
+
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[largestVote]}</div>
+      <div>has {findLargest} votes</div>
+    </div>
   )
 }
-
 export default App
