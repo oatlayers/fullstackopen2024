9112d9b6826f91a5a1d5d04dfe5b1f107dbeb6e8
oatlayers <71181864+oatlayers@users.noreply.github.com>
2023-08-12 20:50:08 +0700

    progress

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,24 @@
+import { useStat, useState } from "react"
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submmit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
