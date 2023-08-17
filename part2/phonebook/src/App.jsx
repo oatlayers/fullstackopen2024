e541ddd6159d63fc29e49994234bb0dcdac8b09f
oatlayers <71181864+oatlayers@users.noreply.github.com>
2023-08-17 12:48:56 +0700

    2.6 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,47 @@
-import { useStat, useState } from "react"
+import { useState } from "react"
+
+const Name = ({person}) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Alan Turing'}
   ])
   const [newName, setNewName] = useState('')
+  
+  const addNumber = (event) => {
+    event.preventDefault();
+    const newObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newObject))
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNumber}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
-          <button type="submmit">add</button>
+          <button type="submit">add</button>
         </div>
       </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <Name key={person.name} person={person} />
+      )}
     </div>
   )
 }
