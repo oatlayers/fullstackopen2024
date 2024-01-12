aebfd1ac4c3739fe7d3b859d99a6d16a200fae70
oatlayers <71181864+oatlayers@users.noreply.github.com>
2024-01-12 17:29:16 +0700

    finished 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,86 +1,86 @@
 import { useEffect, useState } from "react"
-import countriesService from "./services/countries"
 import axios from 'axios'
 
-const Countries = ({countries}) => {
-
-  // for (const key in countries["languages"]) {
-  //   const value = countries["languages"][key];
-  //   console.log(`${value}`);
-  // }
+const Display = ({country}) => {
 
   return (
     <>
-      <h1>{countries["name"]["common"]}</h1>
-      <p>capital {countries["capital"]}</p>
-      <p>area {countries["area"]}</p>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
 
       <b>languages:</b>
+      <ul>
+        {Object.keys(country.languages).map(keys => <li key={keys}>{country.languages[keys]}</li>)}
+      </ul>
 
-      {console.log(countries["flag"])}
+      <img src={`${country.flags.png}`} alt={`flag of ${country.name.common}`}/>
     </>
-
   )
 }
 
+
 const App = () => {
-  const [countries, setCountries] = useState()
-  const [filter, setFilter] = useState('')
-  const [test, setTest] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+  const [filter, setFilter] = useState([])
+  const [filteredCountry, setFilteredCountry] = useState([])
+  const [country, setCountry] = useState({})
 
-  // useEffect(() => {
-  //   countriesService
-  //   .getAll('india')
-  //   .then(initialCountry => {
-  //     setCountries(initialCountry)
-  //   })
-  // }, [])
-  
-  const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
-
-  // const obj = { France: "Paris", England: "London" };
-  // // Iterate over the property names:
-  // for (const country of Object.keys(obj)) {
-  //   const capital = obj[country];
-  //   console.log(country, capital);
-  // }
-
+  // get initial data and store in countries state
   useEffect(() => {
-    axios.get(baseUrl)
-    .then(response => {
-      setTest(response.data)
-      console.log('value of response data:', response.data)
-      console.log('value of test:', test)
-      console.log('value of test name common:', test[1]["name"]["common"])
-      
+      axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(
+        response => setAllCountries(response.data)
+      )
+    }, [])
 
-    })
-  }, [filter])
+  // set value of filteredcountry to value of filter. !==-1 means it only set the value of filteredcountry to when it returns true (when common name contains the string of filter)
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
 
-  const testFind = (test) => {
-    for (const key in Object.keys(test)) {
+    setFilteredCountry(
+      allCountries.filter(
+        country => country.name.common.toLowerCase().search(event.target.value.toLowerCase()) !== -1
+      )
+    ) 
+  }
 
+// whenever value of filteredCountry changes, effect is triggered and if value is 1 then set country state value to the first array element bc there's only one otherwise return empty object
+  useEffect(() => {
+    console.log('length:', filteredCountry.length)
+    console.log('value of filteredCountry:', filteredCountry)
+
+    if (filteredCountry.length === 1) {
+      setCountry({...filteredCountry[0]})
+    } else if (filteredCountry.length !== 1) {
+      setCountry({})
+    }
+  
+  }, [filteredCountry])
+
+  // display list of matches when less or equal than ten
+  const displayCountry = () => {
+    if (filteredCountry.length > 10) {
+      return (<p>Too many matches, specify another filter</p>)
+    } else if (filteredCountry.length === 1) {
+      return (false)
+    } else if (filteredCountry.length <= 10) {
+      return (Object.keys(filteredCountry).map(
+        keys => <p key={filteredCountry[keys].ccn3}>{filteredCountry[keys].name.common}</p>))
     }
   }
 
-  console.log('value of test map:',
-    test.map(object => object["name"])
-  )
+  console.log('value of country:', country)
 
-  const handleFilter = (event) => {
-    console.log(event.target.value)
-    setFilter(event.target.value)
-  }
-      
-  // console.log('value of filter:', filter)
-
-  // console.log('value of countries common name:', countries["name"]["common"])
-
-
+  // value={filter} bc controlled component. only display last expression when country has value (when filtered country only has one element)
   return (
     <>
-      find countries <input onChange={handleFilter}></input>
+      find countries <input value={filter} onChange={handleFilter}></input>
 
+      {displayCountry()}
+      
+
+      {country.name && <Display country={country}/>}
     </>
   )
 }
