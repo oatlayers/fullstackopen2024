8e40b287044f945e029f92e9b5a52b37c983a11c
oatlayers <71181864+oatlayers@users.noreply.github.com>
2024-02-12 16:59:11 +0700

    finished until part 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,41 +2,31 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = 'mongodb://localhost/bloglist'
-// mongoose.connect(mongoUrl)
-
+mongoose.set('strictQuery', false)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+const blogsRouter = require('./controllers/blogs')
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.requestLogger)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.use('/', blogsRouter)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
