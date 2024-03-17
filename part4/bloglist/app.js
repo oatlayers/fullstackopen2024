57ed86a9615463a2688c6a733ae1d35406b02e1c
oatlayers <71181864+oatlayers@users.noreply.github.com>
2024-03-17 23:45:10 +0700

    finished until 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,6 +9,7 @@ app.use(express.json())
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -18,6 +19,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 logger.info('connecting to', config.MONGODB_URI)
 
