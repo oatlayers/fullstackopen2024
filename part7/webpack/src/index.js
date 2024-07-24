c12a4201f2f03976385e5e02b3e3d58f74148459
unknown <71181864+oatlayers@users.noreply.github.com>
2024-07-24 20:08:05 +0700

    progress on part 7e

--- part7/webpack/src/index.js
+++ part7/webpack/src/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import ReactDOM from 'react-dom/client'
 import 'core-js/stable/index.js'
 import 'regenerator-runtime/runtime.js'
+import './index.css'
 
 const hello = name => {
     console.log(`hello ${name}`)
