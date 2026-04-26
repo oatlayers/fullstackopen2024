143ab90c27712e2a1ca0115bcc47c0f9b30c5b63
unknown <71181864+oatlayers@users.noreply.github.com>
2026-04-26 08:32:04 +0700

    cleanup files

--- part4/notes/controllers/testing.js
+++ part4/notes/controllers/testing.js
@@ -1,12 +1,13 @@
-const router = require('express').Router()
-const Note = require('../models/note')
-const User = require('../models/user')
+const router = require("express").Router();
+const Note = require("../models/note");
+const User = require("../models/user");
 
-router.post('/reset', async (request, response) => {
-  await Note.deleteMany({})
-  await User.deleteMany({})
+router.post("/reset", async (request, response) => {
+  await Note.deleteMany({});
+  await User.deleteMany({});
 
-  response.status(204).end()
-})
+  response.status(204).end();
+  console.log("all notes successfully deleted");
+});
 
-module.exports = router
+module.exports = router;
--- part5/notes-e2e/tests/note_app.spec.js
+++ part5/notes-e2e/tests/note_app.spec.js
@@ -1,67 +1,77 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
-const { loginWith, createNote } = require('./helper')
+const { test, describe, expect, beforeEach } = require("@playwright/test");
+const { loginWith, createNote } = require("./helper");
 
-describe('Note app', () => {
+describe("Note app", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('/api/testing/reset')
-    await request.post('/api/users', {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
       data: {
-        name: 'oatlayers4',
-        username: 'oatlayers4',
-        password: '1234567'
-      }
-    })
+        name: "oatlayers4",
+        username: "oatlayers4",
+        password: "1234567",
+      },
+    });
 
-    await page.goto('/')
-  })
+    await page.goto("/");
+  });
 
-  test('front page can be opened', async ({ page }) => {
-    const locator = await page.getByText('Notes')
-    await expect(locator).toBeVisible()
-    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
-  })
+  test("front page can be opened", async ({ page }) => {
+    const locator = page.getByText("Notes");
+    await expect(locator).toBeVisible();
+    await expect(
+      page.getByText(
+        "Note app, Department of Computer Science, University of Helsinki 2024"
+      )
+    ).toBeVisible();
+  });
 
-  test('user can login with correct credentials', async ({ page }) => {
-    await loginWith(page, 'oatlayers2', '123456')
-  
-    await expect(page.getByText('oatlayers2 logged in')).toBeVisible()
-  })
+  test("user can login with correct credentials", async ({ page }) => {
+    await loginWith(page, "oatlayers2", "123456");
 
-  test('login fails with wrong password', async ({ page }) => {
-    await loginWith(page, 'oatlayers5', 'wrong')
+    await expect(page.getByText("oatlayers2 logged in")).toBeVisible();
+  });
 
-    const errorDiv = await page.locator('.error')
-    await expect(errorDiv).toContainText('Wrong credentials')
-    await expect(errorDiv).toHaveCSS('border-style', 'solid')
-    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
-    await expect(page.getByText('oatlayers5 logged in')).not.toBeVisible()
-  })  
+  test("login fails with wrong password", async ({ page }) => {
+    await loginWith(page, "oatlayers5", "wrong");
 
-  describe('when logged in', () => {
+    const errorDiv = page.locator(".error");
+    await expect(errorDiv).toContainText("Wrong credentials");
+    await expect(errorDiv).toHaveCSS("border-style", "solid");
+    await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+    await expect(page.getByText("oatlayers5 logged in")).not.toBeVisible();
+  });
+
+  describe("when logged in", () => {
     beforeEach(async ({ page }) => {
-      await loginWith(page, 'oatlayers2', '123456')
-    })
+      await loginWith(page, "oatlayers2", "123456");
+    });
 
-    test('a new note can be created', async ({ page }) => {
-      await createNote(page, 'a note created by playwright', true)
-      await expect(page.getByText('a note created by playwright')).toBeVisible()
-    })
+    test("a new note can be created", async ({ page }) => {
+      await createNote(page, "a note created by playwright", true);
+      await expect(
+        page.getByText("a note created by playwright")
+      ).toBeVisible();
+    });
 
-    describe('and a note exists', () => {
+    describe("and a note exists", () => {
       beforeEach(async ({ page }) => {
-        await createNote(page, 'first note', true)
-        await createNote(page, 'second note', true)
-        await createNote(page, 'third note', true)
-      })
-  
-      test('importance can be changed', async ({ page }) => {
-        await page.pause()
-        const otherNoteText = await page.getByText('second note')
-        const otherNoteElement = await otherNoteText.locator('..')
-      
-        await otherNoteElement.getByRole('button', { name: 'make not important' }).click()
-        await expect(otherNoteElement.getByText('make important')).toBeVisible()
-      })
-    })
-  })
-})
+        await createNote(page, "first note", true);
+        await createNote(page, "second note", true);
+        await createNote(page, "third note", true);
+      });
+
+      test("importance can be changed", async ({ page }) => {
+        await page.pause();
+        const otherNoteText = page.getByText("second note");
+        const otherNoteElement = otherNoteText.locator("..");
+
+        await otherNoteElement
+          .getByRole("button", { name: "make not important" })
+          .click();
+        await expect(
+          otherNoteElement.getByText("make important")
+        ).toBeVisible();
+      });
+    });
+  });
+});
